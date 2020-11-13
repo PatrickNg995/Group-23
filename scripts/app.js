ecc1b60131c3874ba7e40b9f6e1f79030a81e4e2
spencerrmorrison <smorrison65@my.bcit.ca>
2020-11-13 08:37:54 -0800

    fixed nav bar icons and read/write location into db

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,17 @@
 
-function readQuote(){
-    db.collection("quotes").doc("01")
-    .onSnapshot(function(snap){
-        console.log(snap.data()); //print doc fields of 01
-        console.log(snap.data().message);
-        document.getElementById("abc").innerText = snap.data().message;
+function getUserLocation(){
+    document.getElementById("go").addEventListener('click', function () {
+        var location = document.getElementById("location").value;
+        console.log(location);
+
+        //If user is signed in save it to firebase db-
+        firebase.auth().onAuthStateChanged(function (user) {
+            db.collection("users").doc(user.uid)
+            .update({
+               "Location": location
+            })
+        })
     })
 }
 
-readQuote();
+getUserLocation();
