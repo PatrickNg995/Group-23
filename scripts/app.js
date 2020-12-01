9d5afe36924167bfba52ff5cdee9d17ec89c3bb9
spencerrmorrison <smorrison65@my.bcit.ca>
2020-11-30 21:42:26 -0800

    Final implementations

--- scripts/app.js
+++ scripts/app.js
@@ -13,9 +13,9 @@ function getUserLocation() {
     })
 }
 
-/** Could not get this to function where these are written into database
+///** Could not get this to function where these are written into database
 
-function getItem(){
+function getItem() {
     document.getElementById("go").addEventListener('click', function () {
         var listItem = document.getElementById("listItem").value;
         console.log(listItem);
@@ -30,12 +30,12 @@ function getItem(){
 }
 getItem();
 
-function getItem2(){
+function getItem2() {
     document.getElementById("go").addEventListener('click', function () {
         var listItem2 = document.getElementById("listItem2").value;
         console.log(listItem2);
 
-        firebase.auth().onAuthStateChanged(function (items) {
+        firebase.auth().onAuthStateChanged(function (Lists) {
             db.collection("Lists").doc(items)
             .update({
                "item2": listItem2
@@ -45,12 +45,12 @@ function getItem2(){
 }
 getItem2();
 
-function getItem3(){
+function getItem3() {
     document.getElementById("go").addEventListener('click', function () {
         var listItem3 = document.getElementById("listItem3").value;
         console.log(listItem3);
 
-        firebase.auth().onAuthStateChanged(function (items) {
+        firebase.auth().onAuthStateChanged(function (Lists) {
             db.collection("Lists").doc(items)
             .update({
                "item3": listItem3
@@ -60,12 +60,12 @@ function getItem3(){
 }
 getItem3();
 
-function getItem4(){
+function getItem4() {
     document.getElementById("go").addEventListener('click', function () {
         var listItem4 = document.getElementById("listItem4").value;
         console.log(listItem4);
 
-        firebase.auth().onAuthStateChanged(function (items) {
+        firebase.auth().onAuthStateChanged(function (user) {
             db.collection("Lists").doc(items)
             .update({
                "item5": listItem4
@@ -75,13 +75,13 @@ function getItem4(){
 }
 getItem4();
 
-function getItem5(){
+function getItem5() {
     document.getElementById("go").addEventListener('click', function () {
         var listItem5 = document.getElementById("listItem5").value;
         console.log(listItem5);
 
-        firebase.auth().onAuthStateChanged(function (items) {
-            db.collection("List").doc(items)
+        firebase.auth().onAuthStateChanged(function (Lists) {
+            db.collection("Lists").doc(items)
             .update({
                "item5": listItem5
             })
@@ -90,12 +90,12 @@ function getItem5(){
 }
 getItem5();
 
-function getItem6(){
+function getItem6() {
     document.getElementById("go").addEventListener('click', function () {
         var listItem6 = document.getElementById("listItem6").value;
         console.log(listItem6);
 
-        firebase.auth().onAuthStateChanged(function (items) {
+        firebase.auth().onAuthStateChanged(function (Lists) {
             db.collection("Lists").doc(items)
             .update({
                "item6": listItem6
@@ -105,12 +105,12 @@ function getItem6(){
 }
 getItem6();
 
-function getItem7(){
+function getItem7() {
     document.getElementById("go").addEventListener('click', function () {
         var listItem7 = document.getElementById("listItem7").value;
-        console.log(listItem3);
+        console.log(listItem7);
 
-        firebase.auth().onAuthStateChanged(function (items) {
+        firebase.auth().onAuthStateChanged(function (Lists) {
             db.collection("Lists").doc(items)
             .update({
                "item7": listItem7
@@ -119,4 +119,4 @@ function getItem7(){
     })
 }
 getItem7();
-*/
+//*/
